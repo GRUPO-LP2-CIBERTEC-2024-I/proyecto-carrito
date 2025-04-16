b07cad56c2e9ba78d9a8e86853cb89946605cc7b
jhonatanQuiñones <jhon2226g@gmail.com>
2025-04-16 11:46:54 -0500

    desactivacion de stripe

--- CARRITOADMIN/src/main/java/com/example/pro/Controllers/PaymentController.java
+++ CARRITOADMIN/src/main/java/com/example/pro/Controllers/PaymentController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pro.DTO.PaymentIntentDTO;
 import com.example.pro.services.Impl.PaymentService;
-import com.stripe.exception.StripeException;
-import com.stripe.model.PaymentIntent;
+
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,26 +21,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class PaymentController {
 	@Autowired
 	private PaymentService paymentService;
-	
-	@PostMapping("/paymentintent")
-	public ResponseEntity<String> paymentintent(@RequestBody PaymentIntentDTO paymentIntentDTO) throws StripeException {
-		PaymentIntent paymentIntent = paymentService.paymentIntentDTO(paymentIntentDTO);
-		String paymenttostring = paymentIntent.toJson();
-		return new ResponseEntity<String>(paymenttostring,HttpStatus.OK);
-	}
-	
-	@PostMapping("/confirm/{id}")
-	public ResponseEntity<String> confirm(@PathVariable String id) throws StripeException {
-		PaymentIntent paymentIntent = paymentService.confirm(id);
-		String paymenttostring = paymentIntent.toJson();
-		return new ResponseEntity<String>(paymenttostring,HttpStatus.OK);
-	}
-	
-	@PostMapping("/cancel/{id}")
-	public ResponseEntity<String> cancel(@PathVariable String id) throws StripeException {
-		PaymentIntent paymentIntent = paymentService.cancel(id);
-		String paymenttostring = paymentIntent.toJson();
-		return new ResponseEntity<String>(paymenttostring,HttpStatus.OK);
-	}
+//	
+//	@PostMapping("/paymentintent")
+//	public ResponseEntity<String> paymentintent(@RequestBody PaymentIntentDTO paymentIntentDTO) throws StripeException {
+//		PaymentIntent paymentIntent = paymentService.paymentIntentDTO(paymentIntentDTO);
+//		String paymenttostring = paymentIntent.toJson();
+//		return new ResponseEntity<String>(paymenttostring,HttpStatus.OK);
+//	}
+//	
+//	@PostMapping("/confirm/{id}")
+//	public ResponseEntity<String> confirm(@PathVariable String id) throws StripeException {
+//		PaymentIntent paymentIntent = paymentService.confirm(id);
+//		String paymenttostring = paymentIntent.toJson();
+//		return new ResponseEntity<String>(paymenttostring,HttpStatus.OK);
+//	}
+//	
+//	@PostMapping("/cancel/{id}")
+//	public ResponseEntity<String> cancel(@PathVariable String id) throws StripeException {
+//		PaymentIntent paymentIntent = paymentService.cancel(id);
+//		String paymenttostring = paymentIntent.toJson();
+//		return new ResponseEntity<String>(paymenttostring,HttpStatus.OK);
+//	}
 	
 }
--- CARRITOADMIN/src/main/java/com/example/pro/services/Impl/PaymentService.java
+++ CARRITOADMIN/src/main/java/com/example/pro/services/Impl/PaymentService.java
@@ -8,9 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.pro.DTO.PaymentIntentDTO;
-import com.stripe.Stripe;
-import com.stripe.exception.StripeException;
-import com.stripe.model.PaymentIntent;
+//import com.stripe.Stripe;
+//import com.stripe.exception.StripeException;
+//import com.stripe.model.PaymentIntent;
 
 @Service
 public class PaymentService {
@@ -18,32 +18,32 @@ public class PaymentService {
 	@Value("${config.stripe.secretkey}")
 	private String secretKey;
 	
-	public PaymentIntent paymentIntentDTO(PaymentIntentDTO intent) throws StripeException {
-		Stripe.apiKey = secretKey;
-		Map<String, Object> body = new HashMap<>();
-		body.put("amount", intent.getAmount());
-		body.put("currency", intent.getCurrency());
-		body.put("description", intent.getDescripcion());
-		ArrayList<?> payment_method_types = new ArrayList<>();
-		body.put("payment_method_types", payment_method_types);
-		
-		return PaymentIntent.create(body);
-	}
-	public PaymentIntent confirm(String id) throws StripeException {
-		Stripe.apiKey = secretKey;
-		PaymentIntent paymentintent = PaymentIntent.retrieve(id);
-		Map<String, Object> body = new HashMap<>();
-		body.put("payment_method", "pm_card_visa");
-		paymentintent.confirm(body);
-		return paymentintent;
-	}
-	public PaymentIntent cancel(String id) throws StripeException {
-		Stripe.apiKey = secretKey;
-		PaymentIntent paymentintent = PaymentIntent.retrieve(id);
-		
-		paymentintent.cancel();
-		return paymentintent;
-	}
+//	public PaymentIntent paymentIntentDTO(PaymentIntentDTO intent) throws StripeException {
+//		Stripe.apiKey = secretKey;
+//		Map<String, Object> body = new HashMap<>();
+//		body.put("amount", intent.getAmount());
+//		body.put("currency", intent.getCurrency());
+//		body.put("description", intent.getDescripcion());
+//		ArrayList<?> payment_method_types = new ArrayList<>();
+//		body.put("payment_method_types", payment_method_types);
+//		
+//		return PaymentIntent.create(body);
+//	}
+//	public PaymentIntent confirm(String id) throws StripeException {
+//		Stripe.apiKey = secretKey;
+//		PaymentIntent paymentintent = PaymentIntent.retrieve(id);
+//		Map<String, Object> body = new HashMap<>();
+//		body.put("payment_method", "pm_card_visa");
+//		paymentintent.confirm(body);
+//		return paymentintent;
+//	}
+//	public PaymentIntent cancel(String id) throws StripeException {
+//		Stripe.apiKey = secretKey;
+//		PaymentIntent paymentintent = PaymentIntent.retrieve(id);
+//		
+//		paymentintent.cancel();
+//		return paymentintent;
+//	}
 	
 	
 }
